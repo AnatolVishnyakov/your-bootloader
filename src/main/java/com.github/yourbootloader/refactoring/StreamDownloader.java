44689081da947a319d6f0fd7475458185bd9a7ac
Anatol V <malfenius@gmail.com>
2021-11-21 14:32:44 +0300

    Сервис скачивания видео (асинхронно)

--- src/main/java/com.github/yourbootloader/refactoring/StreamDownloader.java
+++ src/main/java/com.github/yourbootloader/refactoring/StreamDownloader.java
@@ -1,17 +1,19 @@
 package com.github.yourbootloader.refactoring;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.asynchttpclient.*;
+import org.springframework.util.unit.DataSize;
 
+import java.io.File;
 import java.io.FileOutputStream;
-import java.net.URI;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Slf4j
 public class StreamDownloader {
     private final String url;
     private final DownloadContext ctx;
@@ -20,7 +22,7 @@ public class StreamDownloader {
     private int retries;
 
     public StreamDownloader(String fileName, Map<String, Object> infoDict) {
-        this.url = "https://r4---sn-jvhnu5g-c35s.googlevideo.com/videoplayback?expire=1633303271&ei=h-ZZYcy4MKf57QTY9pf4Cw&ip=109.252.67.101&id=o-ACoHRlnMHUyqOU3Km6m-6t-zHaGfL1inATGy36k4sUwz&itag=251&source=youtube&requiressl=yes&mh=u5&mm=31%2C29&mn=sn-jvhnu5g-c35s%2Csn-jvhnu5g-n8vy&ms=au%2Crdu&mv=m&mvi=4&pcm2cms=yes&pl=22&initcwndbps=1781250&vprv=1&mime=audio%2Fwebm&ns=baBvFgwX-3B8IypmK61I5R4G&gir=yes&clen=80099367&dur=4723.861&lmt=1609712300865486&mt=1633281430&fvip=4&keepalive=yes&fexp=24001373%2C24007246&c=WEB&txp=5532434&n=eIjxKzlssUf91G7E&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRgIhALkxPW9JlSSr49eil0wWkc3TuiXFBeb4TGJ0TX0K2McjAiEA2dVF93yRzO-aqKGf-n_1OvTEpWS3FWh0YBzCp4eH1cw%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRgIhAMCSGEot23CIdZF9m0AsXnaACWFeU6qjF25Os2AE7fi7AiEAo1X1JpfiJ9BJ02dtWsbw_PVxNjwj1AuxDH8Q1Av53zA%3D";
+        this.url = "https://r4---sn-jvhnu5g-c35l.googlevideo.com/videoplayback?expire=1637515460&ei=ZCyaYfHAKoWQyQXm1ZHYAQ&ip=2a00%3A1370%3A8127%3Ad967%3A6da5%3Ad65e%3A7d49%3A7b36&id=o-AIzDJYP_pQIbypMhuBxvxUuWS1VQCAKbsWhKaJqfBqUU&itag=22&source=youtube&requiressl=yes&mh=3L&mm=31%2C29&mn=sn-jvhnu5g-c35l%2Csn-jvhnu5g-n8ve7&ms=au%2Crdu&mv=m&mvi=4&pl=51&initcwndbps=2120000&vprv=1&mime=video%2Fmp4&ns=CSsm_RRY9nwNla8N0FJckPgG&cnr=14&ratebypass=yes&dur=3835.147&lmt=1513857691676377&mt=1637493655&fvip=4&fexp=24001373%2C24007246&c=WEB&n=WJlxaTf3qJCBRi8s&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRgIhALNO4VNGpE7X5r7-DkafmYhLMKkSkpAFUYihv7m_8baZAiEA3a53V5WDvhvMqX060_Srt2_wIUXNZR0S9FT-Dm8mKsU%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRgIhAKArHzOzctiPJxfmgpJUlgeuUnnqzSN7HuOPgYvZTIFnAiEAtn_j7L1lzox4SeN19ppC3OX5LEDsS_jevK9zFMQ7PXQ%3D";
 
         DownloadContext ctx = new DownloadContext();
         ctx.setFileName(fileName);
@@ -58,14 +60,35 @@ public class StreamDownloader {
 
     @SneakyThrows
     public void download() {
-        ReadableByteChannel readableByteChannel = Channels.newChannel(new URI(url).toURL().openStream());
-        FileOutputStream fileOutputStream = new FileOutputStream("D:\\IdeaProjects\\your-bootloader\\src\\main\\resources\\" + UUID.randomUUID() + ".mp3");
-        FileChannel fileChannel = fileOutputStream.getChannel();
+        String filePath = "D:\\IdeaProjects\\your-bootloader\\src\\main\\resources\\" + UUID.randomUUID() + ".mp3";
+        FileOutputStream stream = new FileOutputStream(filePath);
 
-        int pos = 0;
-        while (true) {
-            fileChannel.transferFrom(readableByteChannel, pos, 4 * 1024);
-        }
+        AsyncHttpClient client = Dsl.asyncHttpClient();
+        client.prepareGet(url).execute(new AsyncCompletionHandler<FileOutputStream>() {
+            @Override
+            public State onBodyPartReceived(HttpResponseBodyPart bodyPart) throws Exception {
+                printContentWritten();
+                stream.getChannel().write(bodyPart.getBodyByteBuffer());
+                return State.CONTINUE;
+            }
+
+            @Override
+            public FileOutputStream onCompleted(Response response) {
+                System.out.println("Completed download!");
+                return stream;
+            }
+
+            @SneakyThrows
+            private void printContentWritten() {
+                long fileSize = Files.size(new File(filePath).toPath());
+                DataSize dataSize = DataSize.ofBytes(fileSize);
+                if (dataSize.toMegabytes() == 0) {
+                    log.info(dataSize.toKilobytes() + " Kb");
+                } else {
+                    log.info(dataSize.toMegabytes() + " Mb (" + dataSize.toKilobytes() + " Kb)");
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
