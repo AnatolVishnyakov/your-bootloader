acf716604a72460fecab641016481446e9998fb6
Anatol V <malfenius@gmail.com>
2021-11-28 16:52:05 +0300

    Сервис скачивания видео

--- src/main/java/com.github/yourbootloader/YourBootLoaderApplication.java
+++ src/main/java/com.github/yourbootloader/YourBootLoaderApplication.java
@@ -1,13 +1,11 @@
 package com.github.yourbootloader;
 
-import com.github.yourbootloader.refactoring.StreamDownloader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class YourBootLoaderApplication {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(YourBootLoaderApplication.class, args);
+        SpringApplication.run(YourBootLoaderApplication.class, args);
     }
 }
