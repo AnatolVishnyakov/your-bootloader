f2c54f2c3114ae7fb03a2f71081d0fdb239c8cb3
Anatol V <malfenius@gmail.com>
2021-12-11 16:53:30 +0300

    Сервис скачивания видео

--- src/main/java/com.github/yourbootloader/refactoring/YoutubeUrl.java
+++ src/main/java/com.github/yourbootloader/refactoring/YoutubeUrl.java
@@ -1,18 +1,21 @@
 package com.github.yourbootloader.refactoring;
 
+import lombok.Value;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.String.format;
 
+@Value
 public class YoutubeUrl {
     private static final String YOUTUBE_URL_PATTERN = "^((?:https?://|//)(?:www\\.)?www\\.youtube\\.com/(?:watch|movie)(?:\\?|#!\\?)v=)([0-9A-Za-z_-]{11})$";
     private static final Pattern patternCompile = Pattern.compile(YOUTUBE_URL_PATTERN);
 
-    private String url;
-    private String videoId;
-    private String httpScheme;
-    private String webPageUrl;
+    String url;
+    String videoId;
+    String httpScheme;
+    String webPageUrl;
 
     public YoutubeUrl(String url) {
         this.url = url;
@@ -33,9 +36,5 @@ public class YoutubeUrl {
         // TODO анализ prefer_insecure
         return url.startsWith("https")
                 ? "https" : "http";
-    }
-
-    public String getWebPageUrl() {
-        return webPageUrl;
     }
 }
