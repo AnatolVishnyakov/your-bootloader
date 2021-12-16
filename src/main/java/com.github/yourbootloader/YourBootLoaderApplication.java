da61831b1b623e759a067679b2500578000e9e80
Anatol V <malfenius@gmail.com>
2021-12-16 14:11:35 +0300

    Рефакторинг

--- src/main/java/com.github/yourbootloader/YourBootLoaderApplication.java
+++ src/main/java/com.github/yourbootloader/YourBootLoaderApplication.java
@@ -3,20 +3,11 @@ package com.github.yourbootloader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ApplicationContext;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class YourBootLoaderApplication {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(YourBootLoaderApplication.class, args);
-//        String url = "https://www.youtube.com/watch?v=nui3hXzcbK0";
-//        YoutubePageParser youtubePageParser = context.getBean(YoutubePageParser.class, url);
-//        List<Map<String, Object>> formats = youtubePageParser.parse();
-//
-//        Map<String, Object> format = formats.get(2);
-//        System.out.println(DataSize.ofBytes(((Integer) format.get("filesize")).longValue()).toMegabytes() + " Mb");
-//        StreamDownloader downloader = context.getBean(StreamDownloader.class, format.get("url"), UUID.randomUUID().toString(), Collections.emptyMap());
-//        downloader.realDownload(3);
+        SpringApplication.run(YourBootLoaderApplication.class, args);
     }
 }
