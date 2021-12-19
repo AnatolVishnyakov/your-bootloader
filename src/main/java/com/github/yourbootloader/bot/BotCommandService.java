a05d4616adf6566b41d49d3674b5e8259291da96
Anatol V <malfenius@gmail.com>
2021-12-19 20:37:13 +0300

    Добавил уведомление в телеграм чат о кол-ве скачанного содержимого

--- src/main/java/com/github/yourbootloader/bot/BotCommandService.java
+++ src/main/java/com/github/yourbootloader/bot/BotCommandService.java
@@ -1,16 +1,24 @@
 package com.github.yourbootloader.bot;
 
 import com.github.yourbootloader.bot.event.ProgressIndicatorEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.util.unit.DataSize;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class BotCommandService {
+
+    private final Bot bot;
+
     @EventListener
-    public void onProgressIndicatorEvent(ProgressIndicatorEvent event) {
+    public void onProgressIndicatorEventLogging(ProgressIndicatorEvent event) {
         long fileSize = event.getFileSize();
         int receivedLength = event.getBlockSize();
 
@@ -20,6 +28,20 @@ public class BotCommandService {
             log.info("{} Kb ({}) block_size: {}", DataSize.ofBytes(fileSize).toKilobytes(), DataSize.ofBytes(fileSize), receivedLength);
         } else {
             log.info("{} Mb ({}) block_size: {}", DataSize.ofBytes(fileSize).toMegabytes(), DataSize.ofBytes(fileSize), receivedLength);
+        }
+    }
+
+    @EventListener
+    public void onProgressIndicatorEvent(ProgressIndicatorEvent event) {
+        DataSize dataSize = DataSize.ofBytes(event.getFileSize());
+        long downloadedContent = dataSize.toMegabytes() == 0
+                ? dataSize.toKilobytes()
+                : dataSize.toMegabytes();
+        EditMessageText message = new EditMessageText("Скачано " + downloadedContent);
+        try {
+            bot.execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Возникла непредвиденная ошибка", e);
         }
     }
 }
