69c32d9ac216b1d763422a92a35800660b2ffecc
Anatol V <malfenius@gmail.com>
2021-12-26 18:02:45 +0300

    Рефакторинг парсинга страницы

--- src/main/java/com/github/yourbootloader/yt/extractor/YoutubeIE.java
+++ src/main/java/com/github/yourbootloader/yt/extractor/YoutubeIE.java
@@ -70,6 +70,39 @@ public class YoutubeIE extends YoutubeBaseInfoExtractor {
             "(?:www\\.)?grwp24hodrefzvjjuccrkw3mjq4tzhaaq32amf33dzpmuxe7ilepcmad\\.onion",
             "(?:www\\.)?hpniueoejy4opn7bc4ftgazyqjoeqwlvh2uiku2xqku6zpoa4bf5ruid\\.onion"
     );
+    public static String _VALID_URL = "(?x)^" +
+            "                     (" +
+            "                         (?:https?://|//)                                    # http(s):// or protocol-independent URL" +
+            "                         (?:(?:(?:(?:\\w+\\.)?[yY][oO][uU][tT][uU][bB][eE](?:-nocookie|kids)?\\.com|" +
+            "                            (?:www\\.)?deturl\\.com/www\\.youtube\\.com|" +
+            "                            (?:www\\.)?pwnyoutube\\.com|" +
+            "                            (?:www\\.)?hooktube\\.com|" +
+            "                            (?:www\\.)?yourepeat\\.com|" +
+            "                            tube\\.majestyc\\.net|" +
+            "                            %(invidious)s|" +
+            "                            youtube\\.googleapis\\.com)/                        # the various hostnames, with wildcard subdomains" +
+            "                         (?:.*?\\#/)?                                          # handle anchor (#/) redirect urls" +
+            "                         (?:                                                  # the various things that can precede the ID:" +
+            "                             (?:(?:v|embed|e)/(?!videoseries))                # v/ or embed/ or e/" +
+            "                             |(?:                                             # or the v= param in all its forms" +
+            "                                 (?:(?:watch|movie)(?:_popup)?(?:\\.php)?/?)?  # preceding watch(_popup|.php) or nothing (like /?v=xxxx)" +
+            "                                 (?:\\?|\\#!?)                                  # the params delimiter ? or # or #!" +
+            "                                 (?:.*?[&;])??                                # any other preceding param (like /?s=tuff&v=xxxx or ?s=tuff&amp;v=V36LpHqtcDY)" +
+            "                                 v=" +
+            "                             )" +
+            "                         ))" +
+            "                         |(?:" +
+            "                            youtu\\.be|                                        # just youtu.be/xxxx" +
+            "                            vid\\.plus|                                        # or vid.plus/xxxx" +
+            "                            zwearz\\.com/watch|                                # or zwearz.com/watch/xxxx" +
+            "                            %(invidious)s" +
+            "                         )/" +
+            "                         |(?:www\\.)?cleanvideosearch\\.com/media/action/yt/watch\\?videoId=" +
+            "                         )" +
+            "                     )?                                                       # all until now is optional -> you can pass the naked ID" +
+            "                     (?P<id>[0-9A-Za-z_-]{11})                                # here is it! the YouTube video ID" +
+            "                     (?(1).+)?                                                # if we found the ID, everything can follow" +
+            "                     $";
 
     @Autowired
     protected YoutubeIE(YoutubeDLService youtubeDLService) {
