d44dd9f0ef5ded4918fff7428ea9056e50259d5c
Anatol V <malfenius@gmail.com>
2022-01-25 20:24:58 +0300

    Настройка мониторинга

--- src/main/java/com/github/yourbootloader/model/DownloadErrorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.yourbootloader.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DownloadErrorRepository extends JpaRepository<DownloadErrorEntity, Long> {
-}
