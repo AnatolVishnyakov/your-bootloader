7369a54bf6058bbff4cfe9b521d07abc675b5aa6
Anatol V <malfenius@gmail.com>
2022-01-30 14:13:34 +0300

    Реализация модели данных. Конфигурирование liquibase, hikary, datasource

---
+++ src/liquibase/table/user/users.sql
@@ -0,0 +1,9 @@
+-- liquibase formatted postgresql
+
+-- changeset vish.ap:user
+--create table if not exists users
+--(
+--    id bigserial not null primary key
+--    created_at timestamp with time zone not null,
+--    updated_at timestamp with time zone not null
+--);
