6576e8495f5f6609fa8c6478d86114812a6953f0
Anatol V <malfenius@gmail.com>
2022-02-07 21:54:45 +0300

    Реализация обработчиков команд. Разделил обработчик команды от коллбэка

--- src/main/java/com/github/yourbootloader/bot/command/YtParseUrlCommand.java
+++ src/main/java/com/github/yourbootloader/bot/command/YtParseUrlCommand.java
@@ -6,7 +6,6 @@ import com.github.yourbootloader.bot.BotQueryService;
 import com.github.yourbootloader.bot.command.cache.CommandCache;
 import com.github.yourbootloader.bot.dto.VideoInfoDto;
 import com.github.yourbootloader.domain.users.UsersCommandService;
-import com.github.yourbootloader.yt.exception.MethodNotImplementedException;
 import com.github.yourbootloader.yt.extractor.YoutubeIE;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -48,79 +47,75 @@ public class YtParseUrlCommand implements Command {
     public void handle(Bot bot, Update update) {
         log.info("Youtube download command");
 
-        if (update.hasMessage()) {
-            Long chatId = update.getMessage().getChatId();
-            Message message = update.getMessage();
-            String url = message.getText();
-            log.info("Youtube url: {}", url);
+        Long chatId = update.getMessage().getChatId();
+        Message message = update.getMessage();
+        String url = message.getText();
+        log.info("Youtube url: {}", url);
 
-            Map<String, Object> info = botQueryService.getVideoInfo(url);
-            List<Map<String, Object>> formats = ((List<Map<String, Object>>) info.get("formats"))
-                    .stream()
-                    .sorted(Comparator.comparing(m -> {
-                        String formatNote = (String) m.get("format_note");
-                        if (formatNote.matches("\\d+p")) {
-                            return Integer.parseInt(formatNote.replaceAll("p", ""));
-                        }
-                        return 0;
-                    }))
-                    .collect(Collectors.toList());
+        Map<String, Object> info = botQueryService.getVideoInfo(url);
+        List<Map<String, Object>> formats = ((List<Map<String, Object>>) info.get("formats"))
+                .stream()
+                .sorted(Comparator.comparing(m -> {
+                    String formatNote = (String) m.get("format_note");
+                    if (formatNote.matches("\\d+p")) {
+                        return Integer.parseInt(formatNote.replaceAll("p", ""));
+                    }
+                    return 0;
+                }))
+                .collect(Collectors.toList());
 
-            SendMessage sendMessage = new SendMessage();
-            sendMessage.setChatId(message.getChatId().toString());
-            sendMessage.setText("<->");
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText("<->");
 
-            List<VideoInfoDto> videosInfo = new ArrayList<>();
-            InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-            List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<VideoInfoDto> videosInfo = new ArrayList<>();
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
 
-            List<InlineKeyboardButton> rowInline = new ArrayList<>();
-            for (int i = 0; i < formats.size(); i++) {
-                if (i % 2 == 0) {
-                    rowsInline.add(rowInline);
-                    rowInline = new ArrayList<>();
-                }
-                Map<String, Object> format = formats.get(i);
-                InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+        for (int i = 0; i < formats.size(); i++) {
+            if (i % 2 == 0) {
+                rowsInline.add(rowInline);
+                rowInline = new ArrayList<>();
+            }
+            Map<String, Object> format = formats.get(i);
+            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
 
-                String filesizeInString;
-                DataSize filesize = DataSize.ofBytes(Optional.ofNullable(((Long) format.getOrDefault("filesize", 0))).orElse(0L));
-                if (filesize.toMegabytes() > 0) {
-                    filesizeInString = filesize.toMegabytes() + " Mb";
-                } else {
-                    filesizeInString = filesize.toKilobytes() + " Kb";
-                }
-                inlineKeyboardButton.setText(String.format("%s [%s / %s]", format.get("format_note"), format.get("ext"), filesizeInString));
-                JSONObject jsonObject = new JSONObject();
+            String filesizeInString;
+            DataSize filesize = DataSize.ofBytes(Optional.ofNullable(((Long) format.getOrDefault("filesize", 0))).orElse(0L));
+            if (filesize.toMegabytes() > 0) {
+                filesizeInString = filesize.toMegabytes() + " Mb";
+            } else {
+                filesizeInString = filesize.toKilobytes() + " Kb";
+            }
+            inlineKeyboardButton.setText(String.format("%s [%s / %s]", format.get("format_note"), format.get("ext"), filesizeInString));
+            JSONObject jsonObject = new JSONObject();
 //                jsonObject.put("id", UUID.randomUUID());
-                jsonObject.put("format_id", format.get("format_id"));
-                jsonObject.put("filesize", format.get("filesize"));
-                log.debug("Json: {}", jsonObject);
-                videosInfo.add(
-                        new VideoInfoDto(
-                                ((Integer) format.get("format_id")),
-                                (String) info.get("title"),
-                                ((Long) format.get("filesize")),
-                                ((String) format.get("url"))
-                        )
-                );
+            jsonObject.put("format_id", format.get("format_id"));
+            jsonObject.put("filesize", format.get("filesize"));
+            log.debug("Json: {}", jsonObject);
+            videosInfo.add(
+                    new VideoInfoDto(
+                            ((Integer) format.get("format_id")),
+                            (String) info.get("title"),
+                            ((Long) format.get("filesize")),
+                            ((String) format.get("url"))
+                    )
+            );
 
-                inlineKeyboardButton.setCallbackData(jsonObject.toString());
-                rowInline.add(inlineKeyboardButton);
-            }
+            inlineKeyboardButton.setCallbackData(jsonObject.toString());
+            rowInline.add(inlineKeyboardButton);
+        }
 
-            commandCache.save(chatId, videosInfo);
-            markupInline.setKeyboard(rowsInline);
-            sendMessage.setReplyMarkup(markupInline);
+        commandCache.save(chatId, videosInfo);
+        markupInline.setKeyboard(rowsInline);
+        sendMessage.setReplyMarkup(markupInline);
 
-            try {
-                bot.execute(sendMessage);
-            } catch (TelegramApiException e) {
-                log.error("Unexpected error", e);
-                sendNotification(bot, update.getMessage().getChatId(), "Повторите отправку url!");
-            }
-        } else if (update.hasCallbackQuery()) {
-            throw new MethodNotImplementedException();
+        try {
+            bot.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Unexpected error", e);
+            sendNotification(bot, update.getMessage().getChatId(), "Повторите отправку url!");
         }
     }
 
