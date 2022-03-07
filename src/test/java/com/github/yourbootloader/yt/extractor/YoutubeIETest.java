77fc1344a6cebf050b7d0e54ff173f397f1384d5
Anatol V <malfenius@gmail.com>
2022-03-07 20:23:29 +0300

    Рефакторинг

--- src/test/java/com/github/yourbootloader/yt/extractor/YoutubeIETest.java
+++ src/test/java/com/github/yourbootloader/yt/extractor/YoutubeIETest.java
@@ -41,5 +41,24 @@ class YoutubeIETest {
         assertEquals("OCJi5hVdiZU", ytVideoInfo.getId());
         assertEquals("Architects - \"Animals\" (Orchestral Version) - Live at Abbey Road", ytVideoInfo.getTitle());
         assertEquals("\"Animals\" (Orchestral Version) by Architects, recorded live at Abbey Road Studios for Amazon Originals. Listen Here: https://architects.ffm.to/amazonoriginal/amazon\n\n\"WARNING: This video has been identified to potentially trigger seizures for people with photosensitive epilepsy. Viewer discretion is advised.\"\n\nArranged and orchestrated by Rosie Danvers\n\nOriginal Version of \"Animals\" by @Architects\u200b from the album 'For Those That Wish To Exist,' available now\nListen to the full album: http://bit.ly/3dVrqVB\u200b\nOrder at https://architects.ffm.to/fttwte\n\nLyrics\n\nI do my best but everything seems ominous\nNot feeling blessed, quite the opposite\nThis shouldn’t feel so monotonous\nIt never rains but it pours\n \nWe’re just a bunch of fucking animals\nBut we’re afraid of the outcome\nDon’t cry to me because the fiction that we’re living in\nSays I should pull the pin\n \nShould I just pull the pin?\n \nI dug my heels\nI thought that I could stop the rot\nThe ground gave way\nNow I’ve lost the plot\nFucked it again\nThat was all I’ve got\nIt never rains but it pours\n \nLife is just a dream within a….\n \nBuried under dirt\nA diamond in the mud\nInfinity is waiting there\n‘Cause nobody can burn a glass cathedral\n \n….dream within a dream within a….\n\nOfficial Site: http://www.architectsofficial.com/\u200b\nFacebook: http://www.facebook.com/architectsuk\u200b\nTwitter: http://www.twitter.com/architectsuk\u200b\nInstagram: http://instagram.com/architects\u200b\n\nEpitaph Records is an artist-first indie label founded in Los Angeles by Bad Religion guitarist, Brett Gurewitz.  Early releases from a variety of punk heavyweights helped launch the 90s punk explosion.  Along the way, Epitaph has grown and evolved creatively while sticking to its mission of helping real artists make great recordings on their own terms.\n\n\n\n#Architects #Epitaph #AmazonOriginal", ytVideoInfo.getDescription());
+        assertEquals("2021-03-25", ytVideoInfo.getUploadDate());
+        assertEquals("Architects", ytVideoInfo.getUploader());
+//        assertEquals("wearearchitects", ytVideoInfo.getUploaderId());
+        assertEquals("http://www.youtube.com/user/wearearchitects", ytVideoInfo.getUploaderUrl());
+        assertEquals("UCdp-kaIi7YO2WmNQ-LafmpA", ytVideoInfo.getChannelId());
+        assertEquals("https://www.youtube.com/channel/UCdp-kaIi7YO2WmNQ-LafmpA", ytVideoInfo.getChannelUrl());
+        assertEquals(269, ytVideoInfo.getDuration());
+        assertEquals(4301780, ytVideoInfo.getViewCount());
+//        assertEquals("None", ytVideoInfo.getAverageRating());
+        assertEquals(0, ytVideoInfo.getAgeLimit());
+        assertEquals("https://www.youtube.com/watch?v=OCJi5hVdiZU", ytVideoInfo.getWebpageUrl());
+//        assertEquals("None", ytVideoInfo.isLive());
+        assertEquals(109260, ytVideoInfo.getLikeCount());
+        assertEquals("Architects", ytVideoInfo.getChannel());
+        assertEquals("Animals", ytVideoInfo.getTrack());
+        assertEquals("Architects", ytVideoInfo.getArtist());
+        assertEquals("For Those That Wish To Exist", ytVideoInfo.getAlbum());
+        assertEquals("Architects", ytVideoInfo.getCreator());
+        assertEquals("Animals", ytVideoInfo.getAltTitle());
     }
 }
