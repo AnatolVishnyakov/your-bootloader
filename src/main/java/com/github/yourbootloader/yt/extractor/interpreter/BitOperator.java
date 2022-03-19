e023191906b41d94f57a7688e9a270e3f5422ddf
Anatol V <malfenius@gmail.com>
2022-03-19 21:15:48 +0300

    Выделил операторы двоичной арифметики

--- src/main/java/com/github/yourbootloader/yt/extractor/interpreter/BitOperator.java
+++ src/main/java/com/github/yourbootloader/yt/extractor/interpreter/BitOperator.java
@@ -2,78 +2,30 @@ package com.github.yourbootloader.yt.extractor.interpreter;
 
 import lombok.Getter;
 
+import java.util.function.BiFunction;
+
 @Getter
 public enum BitOperator {
-    OR("\\|") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a | b;
-        }
-    },
-    XOR("^") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a ^ b;
-        }
-    },
-    AND("&") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a & b;
-        }
-    },
-    SHIFT_RIGHT(">>") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a >> b;
-        }
-    },
-    SHIFT_LEFT("<<") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a << b;
-        }
-    },
-    SUBTRACT("-") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a - b;
-        }
-    },
-    ADD("\\+") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a + b;
-        }
-    },
-    MOD("%") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a % b;
-        }
-    },
-    DIVIDE("/") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a / b;
-        }
-    },
-    MULTIPLY("\\*") {
-        @Override
-        public Integer calculate(Integer a, Integer b) {
-            return a * b;
-        }
-    };
+    OR("\\|", (a, b) -> a | b),
+    XOR("^", (a, b) -> a ^ b),
+    AND("&", (a, b) -> a & b),
+    SHIFT_RIGHT(">>", (a, b) -> a >> b),
+    SHIFT_LEFT("<<", (a, b) -> a << b),
+    SUBTRACT("-", (a, b) -> a - b),
+    ADD("\\+", Integer::sum),
+    MOD("%", (a, b) -> a % b),
+    DIVIDE("/", (a, b) -> a / b),
+    MULTIPLY("\\*", (a, b) -> a * b);
 
-    private final String op;
+    private final String operator;
+    private final BiFunction<Integer, Integer, Integer> function;
 
-    BitOperator(String op) {
-        this.op = op;
+    BitOperator(String operator, BiFunction<Integer, Integer, Integer> function) {
+        this.operator = operator;
+        this.function = function;
     }
 
     public String calculate(String a, String b) {
-        return String.valueOf(calculate(Integer.parseInt(a), Integer.parseInt(b)));
+        return String.valueOf(function.apply(Integer.parseInt(a), Integer.parseInt(b)));
     }
-
-    public abstract Integer calculate(Integer a, Integer b);
 }
