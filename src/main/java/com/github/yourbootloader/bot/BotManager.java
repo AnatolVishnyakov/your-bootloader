51add49ec6db540daa326be286f90fbf3bf3bfb4
Anatol V <malfenius@gmail.com>
2022-07-22 21:28:02 +0300

    Поправил импорт

--- src/main/java/com/github/yourbootloader/bot/BotManager.java
+++ src/main/java/com/github/yourbootloader/bot/BotManager.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.util.unit.DataSize;
