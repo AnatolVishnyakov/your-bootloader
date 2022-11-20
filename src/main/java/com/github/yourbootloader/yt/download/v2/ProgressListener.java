0790f4bc46b570b21239a24ba483c3fb0d23002a
vishnyakov-ap <malfenius@gmail.com>
2022-11-20 22:36:29 +0300

    Рефакторинг клиента для скачивания

--- src/main/java/com/github/yourbootloader/yt/download/v2/ProgressListener.java
+++ src/main/java/com/github/yourbootloader/yt/download/v2/ProgressListener.java
@@ -1,11 +1,34 @@
 package com.github.yourbootloader.yt.download.v2;
 
+import com.github.yourbootloader.bot.event.FailureDownloadEvent;
+import com.github.yourbootloader.bot.event.FinishDownloadEvent;
+import com.github.yourbootloader.bot.event.ProgressIndicatorEvent;
 import io.netty.handler.codec.http.HttpHeaders;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.asynchttpclient.handler.TransferListener;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.util.unit.DataSize;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+
+import java.io.File;
+import java.util.UUID;
 
 @Slf4j
-class ProgressListener implements TransferListener {
+@RequiredArgsConstructor
+public class ProgressListener implements TransferListener {
+
+    private final ApplicationEventPublisher publisher;
+
+    private final Chat chat;
+
+    private final DataSize contentSize;
+
+    private final File originalFile;
+
+    private final UUID downloadId = UUID.randomUUID();
+
+    private long lastTimeMsReceivedByte;
 
     @Override
     public void onRequestHeadersSent(HttpHeaders headers) {
@@ -19,7 +42,20 @@ class ProgressListener implements TransferListener {
 
     @Override
     public void onBytesReceived(byte[] bytes) {
-        log.info("Bytes received: {}", bytes.length);
+        long currentTimeMillis = System.currentTimeMillis();
+        log.info("Bytes received: {} (delay: {} ms)", bytes.length, currentTimeMillis - lastTimeMsReceivedByte);
+
+        publisher.publishEvent(
+                new ProgressIndicatorEvent(
+                        chat,
+                        contentSize,
+                        originalFile.length(),
+                        bytes.length,
+                        downloadId,
+                        currentTimeMillis - lastTimeMsReceivedByte
+                )
+        );
+        lastTimeMsReceivedByte = currentTimeMillis;
     }
 
     @Override
@@ -30,10 +66,12 @@ class ProgressListener implements TransferListener {
     @Override
     public void onRequestResponseCompleted() {
         log.info("Request or response completed.");
+        publisher.publishEvent(new FinishDownloadEvent(chat, downloadId, originalFile));
     }
 
     @Override
     public void onThrowable(Throwable t) {
         log.error("Was error: {}", t.getMessage());
+        publisher.publishEvent(new FailureDownloadEvent(chat, t));
     }
 }
