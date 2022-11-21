ba103ebbeef11c5f15305ada7a08d1969bd5bdfd
vishnyakov-ap <malfenius@gmail.com>
2022-11-21 10:52:26 +0300

    Рефакторинг клиента для скачивания

--- src/main/java/com/github/yourbootloader/yt/download/YtDownloadClient.java
+++ src/main/java/com/github/yourbootloader/yt/download/YtDownloadClient.java
@@ -1,6 +1,5 @@
 package com.github.yourbootloader.yt.download;
 
-import com.github.yourbootloader.config.YDProperties;
 import com.github.yourbootloader.yt.Utils;
 import com.github.yourbootloader.yt.download.v2.ProgressListener;
 import com.github.yourbootloader.yt.download.v2.YtDownloadAsyncHandler;
@@ -30,11 +29,16 @@ import java.util.concurrent.ThreadLocalRandom;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class YtDownloadClient {
 
-    private static final int DEFAULT_TIMEOUT = 10_000_000;
+    /**
+     * Youtube throttles chunks >~10M;
+     * May be useful for bypassing bandwidth throttling
+     * imposed by a webserver (experimental)
+     */
+    private static final int CHUNK_SIZE_DEFAULT = 10_485_760;
+
     private static final DefaultAsyncHttpClientConfig ASYNC_HTTP_CLIENT_CONFIG = new DefaultAsyncHttpClientConfig.Builder()
-            .setRequestTimeout(DEFAULT_TIMEOUT)
-            .setReadTimeout(DEFAULT_TIMEOUT)
-            .setConnectTimeout(DEFAULT_TIMEOUT)
+            .setReadTimeout(6_000_000)
+            .setConnectTimeout(10_000_000)
             .setThreadPoolName(YtDownloadClient.class.getSimpleName())
             .setHttpClientCodecMaxChunkSize(8_192 * 3)
             .setChunkedFileChunkSize(8_192 * 3)
@@ -47,7 +51,6 @@ public class YtDownloadClient {
             .addChannelOption(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator())
             .build();
 
-    private final YDProperties ydProperties;
     private final TempFileGenerator tempFileGenerator;
     private final ApplicationEventPublisher publisher;
 
@@ -84,18 +87,16 @@ public class YtDownloadClient {
         this.url = url;
 
         DataSize dataSize = DataSize.ofBytes(fileSize);
-        log.info("Размер скачиваемого содержимого: {} Mb ({} Kb)", dataSize.toMegabytes(), dataSize.toKilobytes());
+        log.info("Size content: {} Mb ({} Kb)", dataSize.toMegabytes(), dataSize.toKilobytes());
         File file = tempFileGenerator.create(fileName + "." + dataSize.toBytes());
 
         int start = (int) file.length() == 0 ? 0 : (int) file.length();
-        int end = 0;
 
         HttpHeaders headers = Utils.newHttpHeaders();
-        int chunkSizeDefault = 10_485_760;
         while (start < fileSize) {
-            int chunkSize = ThreadLocalRandom.current().nextInt((int) (chunkSizeDefault * 0.95), chunkSizeDefault);
-            end = (int) Math.min(start + chunkSize - 1, dataSize.toBytes() - 1);
-            log.info("Chunk bytes={}-{} downloading... ", start, end);
+            int chunkSize = ThreadLocalRandom.current().nextInt((int) (CHUNK_SIZE_DEFAULT * 0.95), CHUNK_SIZE_DEFAULT);
+            int end = (int) Math.min(start + chunkSize - 1, dataSize.toBytes() - 1);
+            log.info("Range {}-{} downloading... ", start, end);
 
             establishConnection();
             download(start, end, dataSize, file, headers);
