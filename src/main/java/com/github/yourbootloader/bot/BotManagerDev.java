1624d68d3a9d1c737afb201b27ce2565a8b1ed43
vishnyakov-ap <malfenius@gmail.com>
2022-12-17 23:08:23 +0300

    Revert "Рефакторинг клиента для скачивания"
    This reverts commit dc4bb9e856583fd1d3bfde9e13455984bdb4107b.

---
+++ src/main/java/com/github/yourbootloader/bot/BotManagerDev.java
@@ -0,0 +1,36 @@
+package com.github.yourbootloader.bot;
+
+import com.github.yourbootloader.bot.event.ProgressIndicatorEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.util.unit.DataSize;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+@Slf4j
+@Profile("!prod")
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class BotManagerDev {
+
+    private final AtomicLong prevByteReceived = new AtomicLong();
+    private final AtomicReference<Long> savePrevTime = new AtomicReference<>(System.currentTimeMillis());
+
+    @EventListener
+    public void onProgressIndicatorEventDebug(ProgressIndicatorEvent event) {
+        long currentTime = System.currentTimeMillis();
+        long receivedKBytes = DataSize.ofBytes(event.getFileSize()).toKilobytes();
+        long byteReceived = Math.abs(prevByteReceived.get() - receivedKBytes);
+        log.info("Filesize: {} Kb (speed - {} Kb) time: {} ms",
+                DataSize.ofBytes(event.getFileSize()).toKilobytes(), byteReceived,
+                currentTime - savePrevTime.get()
+        );
+        prevByteReceived.set(receivedKBytes);
+        savePrevTime.set(System.currentTimeMillis());
+    }
+}
