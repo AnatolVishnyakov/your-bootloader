8eef8e904d07d238fb33e1ebe34a7b5aec705369
Anatol V <malfenius@gmail.com>
2023-03-22 22:43:00 +0300

    Рефакторинг. Технические улучшения

--- src/main/java/com/github/yourbootloader/yt/extractor/YoutubeBaseInfoExtractor.java
+++ src/main/java/com/github/yourbootloader/yt/extractor/YoutubeBaseInfoExtractor.java
@@ -4,8 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Arrays;
-import java.util.regex.Pattern;
+import java.util.Collections;
 
 import static java.lang.String.format;
 
@@ -14,27 +13,8 @@ import static java.lang.String.format;
  */
 @Slf4j
 public abstract class YoutubeBaseInfoExtractor extends InfoExtractor {
-    public static String _LOGIN_URL = "https://accounts.google.com/ServiceLogin";
-    public static String _TWOFACTOR_URL = "https://accounts.google.com/signin/challenge";
-    public static String _LOOKUP_URL = "https://accounts.google.com/_/signin/sl/lookup";
-    public static String _CHALLENGE_URL = "https://accounts.google.com/_/signin/sl/challenge";
-    public static String _TFA_URL = "https://accounts.google.com/_/signin/challenge?hl=en&TL={0}";
-    public static String _NETRC_MACHINE = "youtube";
-    // If True it will raise an error if no login info is provided
-    public static boolean _LOGIN_REQUIRED = false;
-    public static Pattern _PLAYLIST_ID_RE = Pattern.compile("(?:(?:PL|LL|EC|UU|FL|RD|UL|TL|PU|OLAK5uy_)[0-9A-Za-z-_]{10,}|RDMM)");
 
-    public static JSONObject _DEFAULT_API_DATA = new JSONObject(
-            "{" +
-                    "    'context': {" +
-                    "        'client': {" +
-                    "            'clientName': 'WEB'," +
-                    "                    'clientVersion': '2.20201021.03.00'," +
-                    "        }" +
-                    "    }," +
-                    "}"
-    );
-    public static String _YT_INITIAL_DATA_RE = "(?:window\\s*\\[\\s*[\"\']ytInitialData[\"\']\\s*\\]|ytInitialData)\\s*=\\s*(\\{.+?})\\s*;";
+    public static String _YT_INITIAL_DATA_RE = "(?:window\\s*\\[\\s*[\"']ytInitialData[\"']\\s*\\]|ytInitialData)\\s*=\\s*(\\{.+?})\\s*;";
     public static String _YT_INITIAL_PLAYER_RESPONSE_RE = "ytInitialPlayerResponse\\s*=\\s*(\\{.+?})\\s*;";
     public static String _YT_INITIAL_BOUNDARY_RE = "(?:var\\s+meta|</script|\n)";
 
@@ -43,35 +23,10 @@ public abstract class YoutubeBaseInfoExtractor extends InfoExtractor {
         super(youtubeDLService);
     }
 
-    // TODO реализовать авторизацию
-    public boolean login() {
-        log.error("Not implemented!");
-        throw new RuntimeException();
-    }
-
-    // TODO реализовать работу с куками
-    public void initializeConsent() {
-        log.error("Not implemented!");
-        throw new RuntimeException();
-    }
-
-    // TODO implement
-    public void realInitialize() {
-        initializeConsent();
-        if (this.downloader == null || !login()) {
-            return;
-        }
-    }
-
-    public void callApi() {
-        log.error("Not implemented!");
-        throw new RuntimeException();
-    }
-
     public JSONObject extractYtInitialData(String videoId, String webPage) {
         return this.parseJson(
                 this.searchRegex(
-                        Arrays.asList(format("%s\\s*%s %s", _YT_INITIAL_DATA_RE, _YT_INITIAL_BOUNDARY_RE, _YT_INITIAL_DATA_RE)), // TODO check pattern count
+                        Collections.singletonList(format("%s\\s*%s %s", _YT_INITIAL_DATA_RE, _YT_INITIAL_BOUNDARY_RE, _YT_INITIAL_DATA_RE)), // TODO check pattern count
                         webPage,
                         "yt initial data"
                 ),
@@ -82,16 +37,11 @@ public abstract class YoutubeBaseInfoExtractor extends InfoExtractor {
     public JSONObject extractYtcfg(String videoId, String webPage) {
         return this.parseJson(
                 this.searchRegex(
-                        Arrays.asList("ytcfg\\.set\\s*\\(\\s*({.+?})\\s*\\)\\s*;"), // TODO check pattern count
+                        Collections.singletonList("ytcfg\\.set\\s*\\(\\s*({.+?})\\s*\\)\\s*;"), // TODO check pattern count
                         webPage,
                         "ytcfg"
                 ),
                 videoId
         );
-    }
-
-    public void extractVideo() {
-        log.error("Not implemented!");
-        throw new RuntimeException();
     }
 }
